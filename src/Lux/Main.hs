938deafc99c789cf5fa9a54b7d45185345db1242
Kristian Glass <git@doismellburning.co.uk>
2014-02-20 19:17:03 +0000

    Pulled out most of the Nagios code

--- src/Lux/Main.hs
+++ src/Lux/Main.hs
@@ -3,11 +3,10 @@ module Lux.Main where
 import Control.Concurrent
 import Control.Monad
 import qualified Control.Concurrent.BoundedChan as BC
-import Data.Text
-import GHC.IO.Exception
 import System.Process
 
 import Lux.Core
+import Lux.Inputs.Nagios
 
 main = do
 			putStrLn "Starting Lux"
@@ -28,25 +27,6 @@ runCheck :: Command -> IO Response
 runCheck (NagiosPlugin fp) = do
 									(code, stdout, _) <- readProcessWithExitCode fp [] ""
 									return $ parseNagiosOutput code stdout
-
-determineStatus :: ExitCode -> Status
-determineStatus ExitSuccess = Ok
-determineStatus (ExitFailure 1) = Warning
-determineStatus (ExitFailure 2) = Critical
-determineStatus _ = undefined -- TODO Handling things outside of scope?
-
-parseNagiosOutput :: ExitCode -> String -> Response
-parseNagiosOutput code stdout =
-	let
-		status = determineStatus code
-		(description, metrics) = parseNagiosOutput2 stdout
-	in Response status description metrics
-
-parseNagiosOutput2 :: String -> (String, [Metric])
-parseNagiosOutput2 stdout =
-	let
-		description = (unpack . strip . pack) stdout
-	in (description, [])
 
 commandThread :: BC.BoundedChan Response -> Command -> IO ThreadId
 commandThread channel command = do
